2b7192de7127f995a2749df0c4da9435122d5e87
lloyd <dailoi280702@gmail.com>
2022-09-27 21:47:38 +0700

    add next-auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,25 @@
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin',
+  },
+  callbacks: {
+    async session({ session, token }) {
+      session.user.uid = token.sub!;
+      // return {
+      //   ...session,
+      //   name: session.user!.name!.split(' ').join('').toLowerCase(),
+      //   uid: token.sub,
+      // };
+      return session;
+    },
+  },
+});
