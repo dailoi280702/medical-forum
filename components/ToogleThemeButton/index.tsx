35552f711d1ce346e2898baed8fa40b9434dd7b8
Vippro67 <Vippro67@gmail.com>
2022-10-27 05:22:51 +0700

    style: update theme toggle button

--- components/ToogleThemeButton/index.tsx
+++ components/ToogleThemeButton/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 const ThemeToggleButton = ({ dark, toggleTheme }: Props) => {
   return (
     <div
-      className='p-2 rounded-lg shadow-md bg-gray-100 dark:bg-neutral-700 dark:text-neutral-100'
+      className='p-2 rounded-lg shadow-md bg-gray-100 dark:bg-neutral-700 dark:text-neutral-50'
       onClick={toggleTheme}
     >
       {dark ? <MoonIcon className='h-7' /> : <SunIcon className='h-7' />}
