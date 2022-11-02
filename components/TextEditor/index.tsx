223bc17a95e7fac40550978937c2a13836f1ce8c
HuynhThienTam <huynhthientam59@gmail.com>
2022-11-02 13:18:56 +0700

    feat(text editor): create editor with quiljs

--- components/TextEditor/index.tsx
+++ components/TextEditor/index.tsx
@@ -1,5 +1,26 @@
-const TextEditor = () => {
-    return <div>this is an editor </div>;
-  };
-  
-  export default TextEditor;
+import 'react-quill/dist/quill.snow.css';
+import { Dispatch, SetStateAction } from 'react';
+
+interface Props {
+  value: string;
+  placeHolder?: string;
+  setValue: Dispatch<SetStateAction<string>>;
+  className?: string;
+}
+
+const TextEditor = ({ value, placeHolder, setValue, className }: Props) => {
+  const ReactQuill =
+    typeof window === 'object' ? require('react-quill') : () => false;
+
+  return (
+    <ReactQuill
+      className={className}
+      value={value}
+      onChange={setValue}
+      placeholder={placeHolder}
+      theme="snow"
+    />
+  );
+};
+
+export default TextEditor;
