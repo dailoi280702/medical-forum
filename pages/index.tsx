b0d643b6eb9f5e4c0b873fc261cbebc61289130f
Lloyd <dailoi280702@gmail.com>
2022-11-11 15:14:46 +0700

    fix: fix issue page have two different color when loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,16 @@ import PostList from 'components/PostList';
 const Home: NextPage = () => {
   return (
     <>
-      <div className='h-screen w-full bg-neutral-100 dark:bg-neutral-800 dark:border- top-0 z-[-1] absolute' />
+      <div className="h-screen w-full bg-neutral-100 dark:bg-neutral-900 dark:border- top-0 z-[-1] absolute" />
       <Head>
         <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <PageWrapper>
         <>
           <CreatePostField />
-          <div className='mt-20'>
+          <div className="mt-20">
             <PostList />
           </div>
         </>
