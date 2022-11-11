ac956d50fe430430e8fa6efb516dea56cff560a5
Triệu Tuấn Tú <90883273+Vippro67@users.noreply.github.com>
2022-11-11 22:02:32 +0700

    feat(post): animate post presence

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -1,16 +1,11 @@
 import { db } from '../../firebase/clientApp';
-import {
-  collection,
-  getCountFromServer,
-  onSnapshot,
-  onSnapshotsInSync,
-  Timestamp,
-} from 'firebase/firestore';
+import { collection, onSnapshot, Timestamp } from 'firebase/firestore';
 import { useState, useEffect } from 'react';
-import Image from 'next/image';
 import PostHead from './PostHead';
 import PostContent from './PostContent';
 import PostTool from './PostTools';
+import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export interface DPost {
   authorId: string;
@@ -18,7 +13,7 @@ export interface DPost {
   authorImg: string;
   title: string;
   html: string;
-  timeStamp: Timestamp;
+  timeStamp: Timestamp | null;
   editDate?: Timestamp;
   numberOfComment?: number;
   numberOfWaitings?: number;
@@ -32,6 +27,7 @@ export interface DInterested {
 }
 
 const Post = ({ id, post }: { id: string; post: DPost }) => {
+  const router = useRouter();
   const [interesteds, setInteresteds] = useState<Map<string, Timestamp>>(
     new Map()
   );
@@ -49,32 +45,42 @@ const Post = ({ id, post }: { id: string; post: DPost }) => {
   );
 
   return (
-    <li className='rounded-lg border cursor-pointer border-neutral-500 dark:border-neutral-600 mb-4 p-4 bg-neutral-50 dark:bg-zinc-800/50'>
-      {/** img, username, edited, sovled **/}
-      <PostHead
-        authorName={post.authorName}
-        authorImg={post.authorImg}
-        timeStamp={post.timeStamp}
-      />
-      <div className='ml-12'>
-        <hr className='my-4 border-neutral-300 dark:border-neutral-600' />
-        <PostContent title={post.title} html={post.html} />
-        <hr className='my-4 border-neutral-300 dark:border-neutral-600' />
-        <PostTool
-          solved={true}
-          numberOfWaitings={0}
-          numberOfComments={0}
-          saved={true}
-          interested={true}
-          savePost={() => {
-            console.log('save clicked');
-          }}
-          setInterested={() => {
-            console.log('set interested clicked');
-          }}
+    <AnimatePresence>
+      <motion.div
+        className='rounded-lg border cursor-pointer border-neutral-500 dark:border-neutral-600 mb-4 p-4 bg-neutral-50 dark:bg-zinc-800/50 list-none'
+        onClick={() => {
+          router.push(`/question/${id}`);
+        }}
+        initial={{ x: -300, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: 300, opacity: 0 }}
+      >
+        {/** img, username, edited, sovled **/}
+        <PostHead
+          authorName={post.authorName}
+          authorImg={post.authorImg}
+          timeStamp={post.timeStamp}
         />
-      </div>
-    </li>
+        <div className='ml-12'>
+          <hr className='my-4 border-neutral-300 dark:border-neutral-600' />
+          <PostContent title={post.title} html={post.html} />
+          <hr className='my-4 border-neutral-300 dark:border-neutral-600' />
+          <PostTool
+            solved={true}
+            numberOfWaitings={0}
+            numberOfComments={0}
+            saved={true}
+            interested={true}
+            savePost={() => {
+              console.log('save clicked');
+            }}
+            setInterested={() => {
+              console.log('set interested clicked');
+            }}
+          />
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
