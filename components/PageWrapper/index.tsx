b87100f5f724828ae4e49ce9f7bc95a6e23eaa53
Triệu Tuấn Tú <90883273+Vippro67@users.noreply.github.com>
2022-11-15 18:57:19 +0700

    fix: page wrapper cannot have nothing

--- components/PageWrapper/index.tsx
+++ components/PageWrapper/index.tsx
@@ -1,4 +1,6 @@
-const PageWrapper = ({ children }: { children: JSX.Element }) => {
+import { ReactNode } from 'react';
+
+const PageWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <div className='flex flex-col items-center bg-neutral-100 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100'>
       <div className='p-4 mx-auto w-full max-w-screen-md'>{children}</div>
