bf762b6b20ddf7168842780418212b3ce3f8aa20
HuynhThienTam <huynhthientam59@gmail.com>
2022-11-15 14:54:53 +0700

    feat(modal): udpate modal template

--- components/PopUpModal/index.tsx
+++ components/PopUpModal/index.tsx
@@ -1,28 +1,19 @@
 import { Dialog } from '@headlessui/react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useRecoilState } from 'recoil';
+import { ReactNode } from 'react';
+import MessageModal from './MessageModal';
+import ErrorModal from './ErrorModal';
+import ConfirmModal from './ConfirmModal';
 
 type Props = {
-  title: string;
-  children: JSX.Element;
+  title: ReactNode;
+  children: ReactNode;
+  buttons: ReactNode;
   open: boolean;
   onClose: () => void;
-  closeText?: string;
-  confirmText?: string;
-  onDone?: () => void;
-  isWarning?: boolean;
 };
 
-const PopupModal = ({
-  title,
-  children,
-  open,
-  onClose,
-  closeText,
-  confirmText,
-  onDone,
-  isWarning,
-}: Props) => {
+const PopupModal = ({ title, children, buttons, open, onClose }: Props) => {
   return (
     <AnimatePresence>
       {open && (
@@ -35,37 +26,18 @@ const PopupModal = ({
           open={open}
           onClose={onClose}
         >
-          <div className='backdrop-blur-sm fixed inset-0 bg-neutral-900/30 darK:bg-neutral-700/30 flex items-center justify-center'>
+          <div className='backdrop-blur-sm fixed inset-0 bg-neutral-900/30 dark:bg-neutral-700/30 flex items-center justify-center'>
             <Dialog.Panel
-              className='flex flex-col bg-neutral-50/80 dark:bg-neutral-900/80 text-neutral-900 dark:text-neutral-100 p-6 rounded-[1.75rem] min-w-[280px] max-w-[580px]'
+              className='bg-neutral-50/80 dark:bg-neutral-900/80 text-neutral-900 dark:text-neutral-100 p-6 rounded-[1.75rem] min-w-[280px] max-w-[580px]'
               as={motion.div}
               initial={{ scaleY: 0 }}
               animate={{ scaleY: 1 }}
               exit={{ scaleY: 0.0 }}
             >
-              <Dialog.Title className='text-2xl mb-4'>{title}</Dialog.Title>
-              <div>{children}</div>
-              <div className='self-end mt-6'>
-                <button
-                  className={isWarning ? 'red-text-button' : 'blue-text-button'}
-                  onClick={onClose}
-                >
-                  {closeText ? closeText : 'Got It'}
-                </button>
-                {confirmText && onDone && (
-                  <button
-                    className={
-                      (isWarning ? 'red-text-button' : 'blue-text-button') +
-                      'ml-4'
-                    }
-                    onClick={() => {
-                      onClose();
-                      onDone();
-                    }}
-                  >
-                    {confirmText}
-                  </button>
-                )}
+              <div className='text-2xl mb-4'>{title}</div>
+              <div className='text-sm'>{children}</div>
+              <div className='flex items-center justify-end space-x-2 mt-6'>
+                {buttons}
               </div>
             </Dialog.Panel>
           </div>
@@ -74,5 +46,7 @@ const PopupModal = ({
     </AnimatePresence>
   );
 };
+
+export { MessageModal, ErrorModal, ConfirmModal };
 
 export default PopupModal;
