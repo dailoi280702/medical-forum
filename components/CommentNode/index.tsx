484bbfeabe7ea1b10971147ae02f76349dcff7d9
HuynhThienTam <huynhthientam59@gmail.com>
2022-11-26 21:27:36 +0700

    feat: update post and comment ui

--- components/CommentNode/index.tsx
+++ components/CommentNode/index.tsx
@@ -1,8 +1,11 @@
-import { useContext } from 'react';
-import { createPortal } from 'react-dom';
+import { useContext, useState } from 'react';
 import Comment, { CommentContext } from '../Comment';
 import { CommentTreeContext } from '../CommentTree';
-import { DComment } from '../CreateComment';
+import CreateComment, { DComment } from '../CreateComment';
+import {
+  DeleteCommentProvider,
+  ConfirmDeleteCommentModal,
+} from '../DeleteComment/';
 
 type Props = {
   comment: DComment;
@@ -10,6 +13,7 @@ type Props = {
 };
 
 const CommentNode = ({ id, comment }: Props) => {
+  const [isReplying, setIsReplying] = useState(false);
   const childComments = useContext(CommentTreeContext);
   const child = Array.from(childComments).filter(
     (value) => value[1].parent === id
@@ -17,18 +21,32 @@ const CommentNode = ({ id, comment }: Props) => {
 
   return (
     <CommentContext.Provider value={{ id: id, ...comment }}>
-      <div className="mt-8">
-        <Comment id={id} comment={comment} />
-        <div className="flex">
-          <div className="mx-4 w-0.5 bg-neutral-300 dark:bg-neutral-600" />
-          <div className="w-full">
+      <DeleteCommentProvider>
+        <Comment
+          id={id}
+          comment={comment}
+          onReply={() => setIsReplying((isReplying) => !isReplying)}
+        />
+        <div className='relative'>
+          <div className='py-2 ml-8'>
+            {isReplying && (
+              <CreateComment
+                className='pl-4 pb-8 pt-2'
+                actionButtonText='Reply'
+                setVisibility={(visibility) => setIsReplying(visibility)}
+              />
+            )}
             {child.length > 0 &&
               child.map((value) => (
                 <CommentNode key={value[0]} id={value[0]} comment={value[1]} />
               ))}
           </div>
+          <div className='w-8 flex justify-center absolute left-0 top-0 bottom-0'>
+            <div className='w-0.5 bg-neutral-300 dark:bg-neutral-600' />
+          </div>
         </div>
-      </div>
+        <ConfirmDeleteCommentModal />
+      </DeleteCommentProvider>
     </CommentContext.Provider>
   );
 };
