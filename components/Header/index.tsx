27c6239278921efa523139ce8a44610c25ae82e8
Lloyd <dailoi280702@gmail.com>
2022-12-09 22:13:39 +0700

    fix: header text overflow hidden

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -35,53 +35,53 @@ const Header = () => {
       route: '/',
       text: 'Home',
       isAuthenticationRequired: false,
-      icons: <HomeIcon className="header-nav-icon" />,
+      icons: <HomeIcon className='header-nav-icon' />,
     },
     {
       route: '/your_questions',
       text: 'Your Quesitons',
       isAuthenticationRequired: true,
-      icons: <WindowIcon className="header-nav-icon" />,
+      icons: <WindowIcon className='header-nav-icon' />,
     },
     {
       route: '/saved_questions',
       text: 'Saved Quesitons',
       isAuthenticationRequired: true,
-      icons: <BookmarkIcon className="header-nav-icon" />,
+      icons: <BookmarkIcon className='header-nav-icon' />,
     },
     {
       route: '/waiting_list',
       text: 'Waiting List',
       isAuthenticationRequired: true,
-      icons: <ClockIcon className="header-nav-icon" />,
+      icons: <ClockIcon className='header-nav-icon' />,
     },
   ];
 
   return (
-    <nav className="sticky overflow-hidden top-0 w-full min-w-min z-20 bg-white/80 text-neutral-900  p-4 shadow-sm transition-all dark:shadow-md dark:text-neutral-100 dark:bg-neutral-800/80 backdrop-blur-sm">
-      <div className="flex flex-col items-start h-full max-w-6xl mx-auto text-md md:flex md:items-center md:flex-row md:space-x-8">
-        <div className="flex justify-between items-center w-full md:w-fit">
+    <nav className='sticky overflow-hidden top-0 w-full z-20 bg-white/80 text-neutral-900  p-4 shadow-sm transition-all dark:shadow-md dark:text-neutral-100 dark:bg-neutral-800/80 backdrop-blur-sm'>
+      <div className='flex flex-col items-start h-full max-w-4xl mx-auto text-md md:flex md:items-center md:flex-row md:space-x-3'>
+        <div className='flex justify-between items-center w-full md:w-fit'>
           <h1
-            className="text-3xl text-blue-500 font-bold truncate drop-shadow-lg shadow-black dark:text-blue-400 cursor-pointer"
+            className='text-3xl text-blue-500 font-bold truncate drop-shadow-lg shadow-black dark:text-blue-400 cursor-pointer'
             onClick={() => router.push('/')}
           >
             Medical Forum
           </h1>
-          <div className="flex items-center space-x-4 md:hidden">
+          <div className='flex items-center space-x-4 md:hidden'>
             <ThemeToggleButton dark={dark} toggleTheme={toggleTheme} />
             <motion.button
               whileTap={{ scale: 0.95 }}
               whileHover={{ scale: 1.05 }}
             >
-              <Bars3Icon className="h-8" onClick={() => setOpen(!open)} />
+              <Bars3Icon className='h-8' onClick={() => setOpen(!open)} />
             </motion.button>
           </div>
         </div>
         <AnimatePresence>
           <motion.div
-            layout="size"
+            layout='size'
             transition={{ type: 'spring', bounce: 0.4 }}
-            className={`space-y-6 my-8 px-4 md:flex items-center md:my-0 md:space-y-0 md:space-x-6 truncate w-full md:w-fit ${
+            className={`space-y-6 my-8 px-4 md:flex items-center md:my-0 md:space-y-0 md:space-x-5 truncate w-full md:w-fit ${
               !open ? 'hidden' : ''
             }`}
           >
@@ -90,7 +90,7 @@ const Header = () => {
               .map((route) => (
                 <div
                   key={route.route}
-                  className="header-nav-item"
+                  className='header-nav-item'
                   onClick={() => router.push(route.route)}
                 >
                   <div>{route.icons}</div>
@@ -107,43 +107,43 @@ const Header = () => {
               ))}
             {session ? (
               <>
-                <div className="header-nav-item md:hidden hover:text-red-600 dark:hover:text-red-400">
-                  <ArrowRightOnRectangleIcon className="header-nav-icon" />
+                <div className='header-nav-item md:hidden hover:text-red-600 dark:hover:text-red-400'>
+                  <ArrowRightOnRectangleIcon className='header-nav-icon' />
                   <button onClick={() => signOut()}>
-                    <h3 className="flex-1">Sign Out</h3>
+                    <h3 className='flex-1'>Sign Out</h3>
                   </button>
                 </div>
-                <div className="flex items-center pt-5 border border-b-0 border-x-0 border-t-neutral-200 dark:border-t-neutral-700 md:hidden">
-                  <div className="relative rounded-full w-10 h-10 overflow-hidden mr-4">
+                <div className='flex items-center pt-5 border border-b-0 border-x-0 border-t-neutral-200 dark:border-t-neutral-700 md:hidden'>
+                  <div className='relative rounded-full w-10 h-10 overflow-hidden mr-4'>
                     <Image
-                      className="rounded-full object-cover overflow-hidden"
+                      className='rounded-full object-cover overflow-hidden'
                       src={session.user.image}
                       alt={session.user.name}
                       fill
                     />
                   </div>
-                  <p className="text-sm">{session.user.name}</p>
+                  <p className='text-sm'>{session.user.name}</p>
                 </div>
               </>
             ) : (
-              <div className="header-nav-item md:hidden hover:text-green-600 dark:hover:text-green-400">
-                <ArrowLeftOnRectangleIcon className="header-nav-icon" />
+              <div className='header-nav-item md:hidden hover:text-green-600 dark:hover:text-green-400'>
+                <ArrowLeftOnRectangleIcon className='header-nav-icon' />
                 <button onClick={() => signIn()}>
-                  <h3 className="flex-1">Sign In</h3>
+                  <h3 className='flex-1'>Sign In</h3>
                 </button>
               </div>
             )}
           </motion.div>
         </AnimatePresence>
-        <span className="flex-1" />
-        <div className="hidden md:inline-flex">
+        <span className='flex-1' />
+        <div className='hidden md:inline-flex'>
           <ThemeToggleButton dark={dark} toggleTheme={toggleTheme} />
         </div>
         {session ? (
-          <div className="group flex items-center space-x-4">
-            <div className="relative rounded-full w-12 h-12 hidden md:inline-flex">
+          <div className='group flex items-center space-x-4'>
+            <div className='relative rounded-full w-12 h-12 hidden md:inline-flex'>
               <Image
-                className="rounded-full object-cover overflow-hidden"
+                className='rounded-full object-cover overflow-hidden'
                 src={session.user.image}
                 alt={session.user.name}
                 fill
@@ -151,14 +151,14 @@ const Header = () => {
             </div>
             <button
               onClick={() => signOut()}
-              className="hidden group-hover:inline-flex header-nav-item hover:text-red-600 dark:hover:text-red-400"
+              className='hidden group-hover:inline-flex header-nav-item hover:text-red-600 dark:hover:text-red-400'
             >
               Sign Out
             </button>
           </div>
         ) : (
           <motion.button
-            className="hidden md:inline-flex hover:text-green-600 dark:hover:text-green-400"
+            className='hidden md:inline-flex hover:text-green-600 dark:hover:text-green-400'
             whileHover={{
               scale: 1.05,
             }}
--- components/PageWrapper/index.tsx
+++ components/PageWrapper/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 const PageWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='flex flex-col items-center bg-neutral-100 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100'>
+    <div className='bg-neutral-100 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100'>
       <div className='p-4 mx-auto w-full max-w-screen-md'>{children}</div>
     </div>
   );
